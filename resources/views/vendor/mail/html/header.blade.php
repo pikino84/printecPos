70b0801a7219ee12397855ba7c04ae184503db3f
pikino84 <pikino84@gmail.com>
2025-04-06 01:14:52 -0500

    actualizacion de la ruta logo mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ config('app.url') }}" style="display: inline-block;">
-            <img src="{{ config('app.url') }}/images/logo-printec.png" alt="Logo Printec" height="50">
+            <img src="{{ config('app.url') }}/images/logo_printec_white.png" alt="Logo Printec" height="50">
         </a>    
     </td>
 </tr>
